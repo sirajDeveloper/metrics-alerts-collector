d3b67a10b6926c48805f3628faaccab6bf206e88
siraj <home-luxe@yandex.ru>
2025-10-12 20:07:31 +0500

    fix

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -11,7 +11,7 @@ var (
 )
 
 func ParseFlags() {
-	flag.StringVar(&Port, "port", ":8080", "address and port to run server")
+	flag.StringVar(&Port, "port", "8080", "address and port to run server")
 	flag.IntVar(&PollInterval, "pollInterval", 2, "poll interval in seconds")
 	flag.IntVar(&ReportInterval, "reportInterval", 10, "report interval in seconds")
 	flag.Parse()
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -23,7 +23,7 @@ func main() {
 		Handler: chiRouter.Handler(),
 	}
 
-	log.Println("Server starting on http://localhost:8080")
+	log.Println("Server starting on http://localhost:" + port)
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatal("Server failed to start:", err)
 	}
