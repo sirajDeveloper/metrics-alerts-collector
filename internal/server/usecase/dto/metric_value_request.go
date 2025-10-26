6acffb0a18be3cb4ce38c14610b20149dcea6ea4
siraj <home-luxe@yandex.ru>
2025-10-26 14:15:07 +0500

    switch request to json

---
+++ internal/server/usecase/dto/metric_value_request.go
@@ -0,0 +1,8 @@
+package dto
+
+type MetricsRequest struct {
+	ID    string  `json:"id"`
+	MType string  `json:"type"`
+	Delta int64   `json:"delta,omitempty"`
+	Value float64 `json:"value,omitempty"`
+}
