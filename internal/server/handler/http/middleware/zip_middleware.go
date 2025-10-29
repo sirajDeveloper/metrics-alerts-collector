a5263d8f160eb4f39add08a3106e63f3b1149e60
siraj <home-luxe@yandex.ru>
2025-10-29 17:38:20 +0500

    update

--- internal/server/handler/http/middleware/zip_middleware.go
+++ internal/server/handler/http/middleware/zip_middleware.go
@@ -6,36 +6,28 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/sirajDeveloper/metrics-alerts-collector/internal/logger"
-	"go.uber.org/zap"
-
 	"github.com/sirajDeveloper/metrics-alerts-collector/internal/server/infrastructure/compress"
 )
 
 func GzipMiddleware(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		contentType, _, err := mime.ParseMediaType(r.Header.Get(compress.ContentType))
-		if err != nil {
-			logger.Log.Error("mime.ParseMediaType has been finished with error. compress won't complete", zap.Error(err))
-			h.ServeHTTP(w, r)
-			return
-		}
-		allowContentType := contentType == compress.ContentTypeJSON || contentType == compress.ContentTypeHTML
 		acceptEncoding := r.Header.Get(compress.AcceptEncoding)
 		contentEncoding := r.Header.Get(compress.ContentEncoding)
-		if !allowContentType {
-			h.ServeHTTP(w, r)
-			return
-		}
 
 		if strings.Contains(contentEncoding, compress.ContentEncodingGzip) {
-			zr, err := compress.NewGzipReader(r.Body)
-			if err != nil {
-				http.Error(w, "invalid gzip data", http.StatusBadRequest)
-				return
+			contentType, _, err := mime.ParseMediaType(r.Header.Get(compress.ContentType))
+			if err == nil {
+				allowContentType := contentType == compress.ContentTypeJSON || contentType == compress.ContentTypeHTML
+				if allowContentType {
+					zr, err := compress.NewGzipReader(r.Body)
+					if err != nil {
+						http.Error(w, "invalid gzip data", http.StatusBadRequest)
+						return
+					}
+					defer zr.Close()
+					r.Body = io.NopCloser(zr)
+				}
 			}
-			defer zr.Close()
-			r.Body = io.NopCloser(zr)
 		}
 
 		if !strings.Contains(acceptEncoding, compress.ContentEncodingGzip) {
